a50dff70f7b37d0320d4d7965a9c3b46986d3db6
jran100 <100c.jran@gmail.com>
2023-05-11 18:01:13 +0900

    init

---
+++ src/healthManagementSystem/Menu.java
@@ -0,0 +1,12 @@
+package healthManagementSystem;
+
+public class Menu {
+	public static void main(String[] args) {
+		System.out.println(
+				"1. Add\n"
+				+ "2. Delete\n"
+				+ "3. Edit\n"
+				+ "4. View\n"
+				+ "5. Exit");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author BAEKJRAN
+ *
+ */
+module HMS {
+}
